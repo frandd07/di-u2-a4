ea156ef42a680c8d71049678bb0328fa68f546c4
frandd07 <fran.doblado.diaz@gmail.com>
2024-11-05 11:06:30 +0100

    Update

--- src/app/RecipeList.js
+++ src/app/RecipeList.js
@@ -2,11 +2,14 @@ import { recipes } from './recipes.js';
 
 export default function RecipeList() {
   const recetas = recipes.map(recipe =>
-    <ul><h2>{recipe.id}</h2>
-    <li>{recipe.name}</li>
-    <li>{recipe.ingredients.join(", ")}</li>
+    <ul key={recipe.name}>
+      <h2>{recipe.name}</h2>
+      {recipe.ingredients.map((ingredient, index) => (
+        <li key={index}>{ingredient}</li>
+      ))}
     </ul>
-  )
+  );
+
   return (
     <div>
       <h1>Recetas</h1>
